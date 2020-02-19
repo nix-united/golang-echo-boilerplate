0299e4d011584d82256df8e41a3e22f729db0b86
Vitalia Smahlenko <vitalia.smahlenko@nixsolutions.com>
2020-02-19 19:01:10 +0200

    Create func for connection wirh bd and seeder

---
+++ server/db/connection.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"echo-demo-project/server/db/seeders"
+	"echo-demo-project/server/models"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"os"
+
+)
+
+func Init() *gorm.DB {
+	dataSourceName := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"))
+	db, err := gorm.Open(os.Getenv("DB_DRIVER"), dataSourceName)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.AutoMigrate(&models.User{}, &models.Post{})
+
+	db.Model(&models.Post{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
+
+	userSeeder := seeders.NewUserSeeder(db)
+	userSeeder.SetUsers()
+
+	return db
+}
---
+++ server/db/seeders/user_seeder.go
@@ -0,0 +1,40 @@
+package seeders
+
+import (
+	"echo-demo-project/server/models"
+	"github.com/jinzhu/gorm"
+)
+
+type UserSeeder struct {
+	DB *gorm.DB
+}
+
+func NewUserSeeder(db *gorm.DB) *UserSeeder {
+	return &UserSeeder{DB: db}
+}
+
+func (userSeeder *UserSeeder) SetUsers() {
+
+	users := map[int]map[string]string{
+		1: map[string]string{
+			"name":     "user1",
+			"password": "password1",
+		},
+		2: map[string]string{
+			"name":     "user2",
+			"password": "password2",
+		},
+	}
+
+	for key, value := range users {
+		user := models.User{}
+		userSeeder.DB.First(&user, key)
+
+		if user.ID == 0 {
+			user.ID = uint(key)
+			user.Name = value["name"]
+			user.Password = value["password"]
+			userSeeder.DB.Create(&user)
+		}
+	}
+}
