2b589e181dd39c10c262bfd5b96ce6dda1628caa
Maksym Kononec <maksym.kononec@nixsolutions.com>
2020-03-31 22:30:51 +0300

    setup build and deploy

--- server/db/connection.go
+++ server/db/connection.go
@@ -7,14 +7,18 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"os"
-
 )
 
 func Init() *gorm.DB {
-	dataSourceName := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"))
+
+	fmt.Println(dataSourceName)
+
 	db, err := gorm.Open(os.Getenv("DB_DRIVER"), dataSourceName)
 	if err != nil {
 		panic(err.Error())
