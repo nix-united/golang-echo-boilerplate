cf610b9680d0968d9c6759d04b1be6041c03091e
Vitalia Smahlenko <vitalia.smahlenko@nixsolutions.com>
2020-04-27 19:36:46 +0300

    Update migrations

--- migrations/entry.go
+++ migrations/entry.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"echo-demo-project/migrations/list"
-	"flag"
 	gm "github.com/ShkrutDenis/go-migrations"
 	gmStore "github.com/ShkrutDenis/go-migrations/store"
 )
 
-var isRollback *bool
-
-func init() {
-	isRollback = flag.Bool("rollback", false, "")
-	flag.Parse()
-}
-
 func main() {
-	if *isRollback {
-		gm.Rollback(getMigrationsList())
-		return
-	}
-
-	gm.Migrate(getMigrationsList())
+	gm.Run(getMigrationsList())
 }
 
 func getMigrationsList() []gmStore.Migratable {
