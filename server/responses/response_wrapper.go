ef90a8305a97e94f98cd02281dc46ec6b63198a8
Oleksandr Brichak <aleksandr.brichak@nixsolutions.com>
2020-05-14 12:09:00 +0300

    Add 'Error' struct to response_wrapper

--- server/responses/response_wrapper.go
+++ server/responses/response_wrapper.go
@@ -6,6 +6,11 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Error struct {
+	Code  int `json:"code"`
+	Error string `json:"error"`
+}
+
 func Response(c echo.Context, statusCode int, data interface{}) error {
 	// nolint // context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	// nolint // context.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
@@ -18,10 +23,7 @@ func SuccessResponse(c echo.Context, data interface{}) error {
 }
 
 func ErrorResponse(c echo.Context, statusCode int, message string) error {
-	return Response(c, statusCode, struct {
-		Code  int
-		Error string
-	}{
+	return Response(c, statusCode, Error{
 		Code:  statusCode,
 		Error: message,
 	})
