ceecf55421ab8cf1f94002e29af34b10f944de93
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-03-07 14:49:08 +0200

    feat: add server shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,11 @@
 package server
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+
 	"github.com/nix-united/golang-echo-boilerplate/internal/config"
 
 	"github.com/labstack/echo/v4"
@@ -25,6 +30,18 @@ func NewServer(
 	}
 }
 
-func (server *Server) Start(addr string) error {
-	return server.Echo.Start(":" + addr)
+func (s *Server) Start(addr string) error {
+	if err := s.Echo.Start(":" + addr); err != nil && errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("start echo: %w", err)
+	}
+
+	return nil
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	if err := s.Echo.Shutdown(ctx); err != nil {
+		return fmt.Errorf("shutdown echo: %w", err)
+	}
+
+	return nil
 }
