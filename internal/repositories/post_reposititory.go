fc39bec0fcbbe3a3af0bf9a29ff430e43d37f0a4
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-05-09 14:36:16 +0300

    refactor: add error to create post func

--- internal/repositories/post_reposititory.go
+++ internal/repositories/post_reposititory.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"fmt"
+
 	"github.com/nix-united/golang-echo-boilerplate/internal/models"
 
 	"gorm.io/gorm"
@@ -14,8 +16,12 @@ func NewPostRepository(db *gorm.DB) PostRepository {
 	return PostRepository{db: db}
 }
 
-func (r PostRepository) Create(post *models.Post) {
-	r.db.Create(post)
+func (r PostRepository) Create(post *models.Post) error {
+	if err := r.db.Create(post).Error; err != nil {
+		return fmt.Errorf("execute insert post query: %w", err)
+	}
+
+	return nil
 }
 
 func (r PostRepository) GetPosts(posts *[]models.Post) {
