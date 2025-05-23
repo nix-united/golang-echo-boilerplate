7387a5122873b823aa3b686a481b93c450994ded
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-05-23 13:33:16 +0300

    test: add post repository tests

--- tests/integration/post_repository_test.go
+++ tests/integration/post_repository_test.go
@@ -26,22 +26,61 @@ func TestPostRepository(t *testing.T) {
 	user, err := userRepository.GetUserByEmail("example_user_email@email.com")
 	require.NotNil(t, user)
 
-	postToCreate := &models.Post{
+	newPost := &models.Post{
 		Title:   "Post title",
 		Content: "Post content",
 		UserID:  user.ID,
 	}
 
-	postRepository.Create(postToCreate)
+	t.Run("It should create a post", func(t *testing.T) {
+		err := postRepository.Create(t.Context(), newPost)
+		require.NoError(t, err)
+		assert.NotZero(t, newPost.ID)
+	})
 
-	gotPosts, err := postRepository.GetPosts()
-	require.NoError(t, err)
-	require.Len(t, gotPosts, 1)
+	t.Run("It should fetch created post", func(t *testing.T) {
+		gotPost, err := postRepository.GetPost(t.Context(), newPost.ID)
+		require.NoError(t, err)
 
-	wantPost := postToCreate
-	wantPost.User = user
-	wantPost.CreatedAt = gotPosts[0].CreatedAt
-	wantPost.UpdatedAt = gotPosts[0].UpdatedAt
+		newPost.CreatedAt = gotPost.CreatedAt
+		newPost.UpdatedAt = gotPost.UpdatedAt
 
-	assert.Equal(t, *postToCreate, gotPosts[0])
+		assert.Equal(t, *newPost, gotPost)
+	})
+
+	t.Run("It should return an error if post not found", func(t *testing.T) {
+		_, err := postRepository.GetPost(t.Context(), 999)
+		assert.ErrorIs(t, err, models.ErrPostNotFound)
+	})
+
+	t.Run("It should fetch all posts", func(t *testing.T) {
+		posts, err := postRepository.GetPosts(t.Context())
+		require.NoError(t, err)
+		require.Len(t, posts, 1)
+		assert.Equal(t, *newPost, posts[0])
+	})
+
+	t.Run("It should update post", func(t *testing.T) {
+		newPost.Title = "New post title"
+		newPost.Content = "New post content"
+		err := postRepository.Update(t.Context(), newPost)
+		require.NoError(t, err)
+
+		gotPost, err := postRepository.GetPost(t.Context(), newPost.ID)
+		require.NoError(t, err)
+
+		newPost.UpdatedAt = gotPost.UpdatedAt
+
+		assert.Equal(t, *newPost, gotPost)
+	})
+
+	t.Run("It should delete post", func(t *testing.T) {
+		id := newPost.ID
+
+		err := postRepository.Delete(t.Context(), newPost)
+		require.NoError(t, err)
+
+		_, err = postRepository.GetPost(t.Context(), id)
+		assert.ErrorIs(t, err, models.ErrPostNotFound)
+	})
 }
