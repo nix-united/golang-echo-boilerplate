8df78b16d219cd938b8d8386704cc87ef96cadd4
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-05-23 14:36:23 +0300

    feat: apply user repo changes

--- internal/services/user/service.go
+++ internal/services/user/service.go
@@ -1,26 +1,30 @@
 package user
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/nix-united/golang-echo-boilerplate/internal/models"
 	"github.com/nix-united/golang-echo-boilerplate/internal/requests"
 	"github.com/nix-united/golang-echo-boilerplate/internal/server/builders"
 
 	"golang.org/x/crypto/bcrypt"
-	"gorm.io/gorm"
 )
 
-type ServiceWrapper interface {
-	Register(request *requests.RegisterRequest) error
+type userRepository interface {
+	Create(ctx context.Context, user *models.User) error
+	GetUserByEmail(ctx context.Context, email string) (models.User, error)
 }
 
-type Service struct {
-	DB *gorm.DB
+type UserService struct {
+	userRepository userRepository
 }
 
-func NewUserService(db *gorm.DB) *Service {
-	return &Service{DB: db}
+func NewUserService(userRepository userRepository) *UserService {
+	return &UserService{userRepository: userRepository}
 }
 
-func (userService *Service) Register(request *requests.RegisterRequest) error {
+func (s *UserService) Register(ctx context.Context, request *requests.RegisterRequest) error {
 	encryptedPassword, err := bcrypt.GenerateFromPassword(
 		[]byte(request.Password),
 		bcrypt.DefaultCost,
@@ -29,12 +33,24 @@ func (userService *Service) Register(request *requests.RegisterRequest) error {
 		return err
 	}
 
-	user := builders.NewUserBuilder().SetEmail(request.Email).
+	user := builders.NewUserBuilder().
+		SetEmail(request.Email).
 		SetName(request.Name).
 		SetPassword(string(encryptedPassword)).
 		Build()
 
-	userService.DB.Create(&user)
+	if err := s.userRepository.Create(ctx, user); err != nil {
+		return fmt.Errorf("create user in repository: %w", err)
+	}
 
 	return nil
+}
+
+func (s *UserService) GetUserByEmail(ctx context.Context, email string) (models.User, error) {
+	user, err := s.userRepository.GetUserByEmail(ctx, email)
+	if err != nil {
+		return models.User{}, fmt.Errorf("get user by email from repository: %w", err)
+	}
+
+	return user, nil
 }
