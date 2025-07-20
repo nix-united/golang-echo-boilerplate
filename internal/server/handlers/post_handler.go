437c5b5e3fa1a2604cada2c04942280e0daccafe
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-07-20 19:09:10 +0300

    test: cover post handler with tests

--- internal/server/handlers/post_handler.go
+++ internal/server/handlers/post_handler.go
@@ -2,18 +2,19 @@ package handlers
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"strconv"
 
 	"github.com/nix-united/golang-echo-boilerplate/internal/models"
 	"github.com/nix-united/golang-echo-boilerplate/internal/requests"
 	"github.com/nix-united/golang-echo-boilerplate/internal/responses"
-	"github.com/nix-united/golang-echo-boilerplate/internal/services/token"
 
 	safecast "github.com/ccoveille/go-safecast"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
+
+//go:generate go tool mockgen -source=$GOFILE -destination=post_handler_mock_test.go -package=${GOPACKAGE}_test -typed=true
 
 type postService interface {
 	Create(ctx context.Context, post *models.Post) error
@@ -27,8 +28,8 @@ type PostHandlers struct {
 	postService postService
 }
 
-func NewPostHandlers(postService postService) PostHandlers {
-	return PostHandlers{postService: postService}
+func NewPostHandlers(postService postService) *PostHandlers {
+	return &PostHandlers{postService: postService}
 }
 
 // CreatePost godoc
@@ -45,6 +46,11 @@ func NewPostHandlers(postService postService) PostHandlers {
 //	@Security		ApiKeyAuth
 //	@Router			/posts [post]
 func (p *PostHandlers) CreatePost(c echo.Context) error {
+	authClaims, err := getAuthClaims(c)
+	if err != nil {
+		return responses.ErrorResponse(c, http.StatusUnauthorized, "Unauthorized")
+	}
+
 	var createPostRequest requests.CreatePostRequest
 	if err := c.Bind(&createPostRequest); err != nil {
 		return responses.ErrorResponse(c, http.StatusBadRequest, "Failed to bind request: "+err.Error())
@@ -54,14 +60,10 @@ func (p *PostHandlers) CreatePost(c echo.Context) error {
 		return responses.ErrorResponse(c, http.StatusBadRequest, "Required fields are empty")
 	}
 
-	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(*token.JwtCustomClaims)
-	id := claims.ID
-
 	post := &models.Post{
 		Title:   createPostRequest.Title,
 		Content: createPostRequest.Content,
-		UserID:  id,
+		UserID:  authClaims.ID,
 	}
 
 	if err := p.postService.Create(c.Request().Context(), post); err != nil {
@@ -69,40 +71,6 @@ func (p *PostHandlers) CreatePost(c echo.Context) error {
 	}
 
 	return responses.MessageResponse(c, http.StatusCreated, "Post successfully created")
-}
-
-// DeletePost godoc
-//
-//	@Summary		Delete post
-//	@Description	Delete post
-//	@ID				posts-delete
-//	@Tags			Posts Actions
-//	@Param			id	path		int	true	"Post ID"
-//	@Success		204	{object}	responses.Data
-//	@Failure		404	{object}	responses.Error
-//	@Security		ApiKeyAuth
-//	@Router			/posts/{id} [delete]
-func (p *PostHandlers) DeletePost(c echo.Context) error {
-	parsedID, err := strconv.ParseUint(c.Param("id"), 10, 64)
-	if err != nil {
-		return responses.ErrorResponse(c, http.StatusBadRequest, "Failed to parse post id: "+err.Error())
-	}
-
-	id, err := safecast.ToUint(parsedID)
-	if err != nil {
-		return responses.ErrorResponse(c, http.StatusBadRequest, "Failed to parse post id: "+err.Error())
-	}
-
-	post, err := p.postService.GetPost(c.Request().Context(), id)
-	if err != nil {
-		return responses.ErrorResponse(c, http.StatusNotFound, "Post not found")
-	}
-
-	if err := p.postService.Delete(c.Request().Context(), &post); err != nil {
-		return responses.ErrorResponse(c, http.StatusInternalServerError, "Failed to delete post: "+err.Error())
-	}
-
-	return responses.MessageResponse(c, http.StatusNoContent, "Post deleted successfully")
 }
 
 // GetPosts godoc
@@ -141,6 +109,11 @@ func (p *PostHandlers) GetPosts(c echo.Context) error {
 //	@Security		ApiKeyAuth
 //	@Router			/posts/{id} [put]
 func (p *PostHandlers) UpdatePost(c echo.Context) error {
+	auth, err := getAuthClaims(c)
+	if err != nil {
+		return responses.ErrorResponse(c, http.StatusUnauthorized, "Unauthorized")
+	}
+
 	parsedID, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
 		return responses.ErrorResponse(c, http.StatusBadRequest, "Failed to parse post id: "+err.Error())
@@ -161,8 +134,14 @@ func (p *PostHandlers) UpdatePost(c echo.Context) error {
 	}
 
 	post, err := p.postService.GetPost(c.Request().Context(), id)
-	if err != nil {
+	if errors.Is(err, models.ErrPostNotFound) {
 		return responses.ErrorResponse(c, http.StatusNotFound, "Post not found")
+	} else if err != nil {
+		return responses.ErrorResponse(c, http.StatusInternalServerError, "Failed to find post: "+err.Error())
+	}
+
+	if post.UserID != auth.ID {
+		return responses.ErrorResponse(c, http.StatusForbidden, "Forbidden")
 	}
 
 	if err := p.postService.Update(c.Request().Context(), &post, updatePostRequest); err != nil {
@@ -170,4 +149,49 @@ func (p *PostHandlers) UpdatePost(c echo.Context) error {
 	}
 
 	return responses.MessageResponse(c, http.StatusOK, "Post successfully updated")
+}
+
+// DeletePost godoc
+//
+//	@Summary		Delete post
+//	@Description	Delete post
+//	@ID				posts-delete
+//	@Tags			Posts Actions
+//	@Param			id	path		int	true	"Post ID"
+//	@Success		204	{object}	responses.Data
+//	@Failure		404	{object}	responses.Error
+//	@Security		ApiKeyAuth
+//	@Router			/posts/{id} [delete]
+func (p *PostHandlers) DeletePost(c echo.Context) error {
+	auth, err := getAuthClaims(c)
+	if err != nil {
+		return responses.ErrorResponse(c, http.StatusUnauthorized, "Unauthorized")
+	}
+
+	parsedID, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		return responses.ErrorResponse(c, http.StatusBadRequest, "Failed to parse post id: "+err.Error())
+	}
+
+	id, err := safecast.ToUint(parsedID)
+	if err != nil {
+		return responses.ErrorResponse(c, http.StatusBadRequest, "Failed to parse post id: "+err.Error())
+	}
+
+	post, err := p.postService.GetPost(c.Request().Context(), id)
+	if errors.Is(err, models.ErrPostNotFound) {
+		return responses.ErrorResponse(c, http.StatusNotFound, "Post not found")
+	} else if err != nil {
+		return responses.ErrorResponse(c, http.StatusInternalServerError, "Failed to find post: "+err.Error())
+	}
+
+	if post.UserID != auth.ID {
+		return responses.ErrorResponse(c, http.StatusForbidden, "Forbidden")
+	}
+
+	if err := p.postService.Delete(c.Request().Context(), &post); err != nil {
+		return responses.ErrorResponse(c, http.StatusInternalServerError, "Failed to delete post: "+err.Error())
+	}
+
+	return responses.MessageResponse(c, http.StatusNoContent, "Post deleted successfully")
 }
