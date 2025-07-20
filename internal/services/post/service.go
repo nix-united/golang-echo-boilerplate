8f7b19aa8c2d4bcc298a1bc18342103edfb768e3
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-07-20 19:31:33 +0300

    refactor: change post service to use pointers receiver

--- internal/services/post/service.go
+++ internal/services/post/service.go
@@ -22,11 +22,11 @@ type Service struct {
 	postRepository postRepository
 }
 
-func NewService(postRepository postRepository) Service {
-	return Service{postRepository: postRepository}
+func NewService(postRepository postRepository) *Service {
+	return &Service{postRepository: postRepository}
 }
 
-func (s Service) Create(ctx context.Context, post *models.Post) error {
+func (s *Service) Create(ctx context.Context, post *models.Post) error {
 	if err := s.postRepository.Create(ctx, post); err != nil {
 		return fmt.Errorf("create post in repository: %w", err)
 	}
@@ -34,7 +34,7 @@ func (s Service) Create(ctx context.Context, post *models.Post) error {
 	return nil
 }
 
-func (s Service) GetPosts(ctx context.Context) ([]models.Post, error) {
+func (s *Service) GetPosts(ctx context.Context) ([]models.Post, error) {
 	posts, err := s.postRepository.GetPosts(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("get posts from repository: %w", err)
@@ -43,7 +43,7 @@ func (s Service) GetPosts(ctx context.Context) ([]models.Post, error) {
 	return posts, nil
 }
 
-func (s Service) GetPost(ctx context.Context, id uint) (models.Post, error) {
+func (s *Service) GetPost(ctx context.Context, id uint) (models.Post, error) {
 	post, err := s.postRepository.GetPost(ctx, id)
 	if err != nil {
 		return models.Post{}, fmt.Errorf("get post from repository: %w", err)
@@ -52,7 +52,7 @@ func (s Service) GetPost(ctx context.Context, id uint) (models.Post, error) {
 	return post, nil
 }
 
-func (s Service) Update(ctx context.Context, post *models.Post, updatePostRequest requests.UpdatePostRequest) error {
+func (s *Service) Update(ctx context.Context, post *models.Post, updatePostRequest requests.UpdatePostRequest) error {
 	post.Content = updatePostRequest.Content
 	post.Title = updatePostRequest.Title
 
@@ -63,7 +63,7 @@ func (s Service) Update(ctx context.Context, post *models.Post, updatePostReques
 	return nil
 }
 
-func (s Service) Delete(ctx context.Context, post *models.Post) error {
+func (s *Service) Delete(ctx context.Context, post *models.Post) error {
 	if err := s.postRepository.Delete(ctx, post); err != nil {
 		return fmt.Errorf("delete post in repository: %w", err)
 	}
