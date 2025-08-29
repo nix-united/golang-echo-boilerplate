02132e01512243023e752ec1c12efd5d87fae96d
oleksandrkhmil <oleksandr.khmil@sumup.com>
2025-08-29 11:28:38 +0300

    refactor: move to main.go bootstrap

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -12,13 +12,23 @@ import (
 	"github.com/nix-united/golang-echo-boilerplate/docs"
 	"github.com/nix-united/golang-echo-boilerplate/internal/config"
 	"github.com/nix-united/golang-echo-boilerplate/internal/db"
+	"github.com/nix-united/golang-echo-boilerplate/internal/repositories"
 	"github.com/nix-united/golang-echo-boilerplate/internal/server"
+	"github.com/nix-united/golang-echo-boilerplate/internal/server/handlers"
 	"github.com/nix-united/golang-echo-boilerplate/internal/server/routes"
+	"github.com/nix-united/golang-echo-boilerplate/internal/services/auth"
+	"github.com/nix-united/golang-echo-boilerplate/internal/services/oauth"
+	"github.com/nix-united/golang-echo-boilerplate/internal/services/post"
+	"github.com/nix-united/golang-echo-boilerplate/internal/services/token"
+	"github.com/nix-united/golang-echo-boilerplate/internal/services/user"
 	"github.com/nix-united/golang-echo-boilerplate/internal/slogx"
 
 	"github.com/caarlos0/env/v11"
+	"github.com/coreos/go-oidc"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
@@ -60,18 +70,65 @@ func run() error {
 		return fmt.Errorf("init logger: %w", err)
 	}
 
+	traceStarter := slogx.NewTraceStarter(uuid.NewV7)
+
 	gormDB, err := db.NewGormDB(cfg.DB)
 	if err != nil {
 		return fmt.Errorf("new db connection: %w", err)
 	}
 
-	app := server.NewServer(echo.New(), gormDB, &cfg)
+	userRepository := repositories.NewUserRepository(gormDB)
+	userService := user.NewService(userRepository)
 
-	err = routes.ConfigureRoutes(slogx.NewTraceStarter(uuid.NewV7), app)
+	postRepository := repositories.NewPostRepository(gormDB)
+	postService := post.NewService(postRepository)
+
+	provider, err := oidc.NewProvider(context.Background(), "https://accounts.google.com")
+	if err != nil {
+		return fmt.Errorf("oidc.NewProvider: %w", err)
+	}
+
+	verifier := provider.Verifier(&oidc.Config{ClientID: cfg.OAuth.ClientID})
+
+	tokenService := token.NewService(
+		time.Now,
+		cfg.Auth.AccessTokenDuration,
+		cfg.Auth.RefreshTokenDuration,
+		[]byte(cfg.Auth.AccessSecret),
+		[]byte(cfg.Auth.RefreshSecret),
+	)
+
+	authService := auth.NewService(userService, tokenService)
+	oAuthService := oauth.NewService(verifier, tokenService, userService)
+
+	postHandler := handlers.NewPostHandlers(postService)
+	authHandler := handlers.NewAuthHandler(authService)
+	oAuthHandler := handlers.NewOAuthHandler(oAuthService)
+	registerHandler := handlers.NewRegisterHandler(userService)
+
+	// Configure middleware with the custom claims type
+	echoJWTConfig := echojwt.Config{
+		NewClaimsFunc: func(echo.Context) jwt.Claims {
+			return new(token.JwtCustomClaims)
+		},
+		SigningKey: []byte(cfg.Auth.AccessSecret),
+	}
+
+	echoJWTMiddleware := echojwt.WithConfig(echoJWTConfig)
+
+	engine := echo.New()
+	err = routes.ConfigureRoutes(traceStarter, engine, routes.Handlers{
+		PostHandler:       postHandler,
+		AuthHandler:       authHandler,
+		OAuthHandler:      oAuthHandler,
+		RegisterHandler:   registerHandler,
+		EchoJWTMiddleware: echoJWTMiddleware,
+	})
 	if err != nil {
 		return fmt.Errorf("configure routes: %w", err)
 	}
 
+	app := server.NewServer(engine)
 	go func() {
 		if err = app.Start(cfg.HTTP.Port); err != nil {
 			slog.Error("Server error", "err", err.Error())
--- internal/server/server.go
+++ internal/server/server.go
@@ -6,32 +6,19 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/nix-united/golang-echo-boilerplate/internal/config"
-
 	"github.com/labstack/echo/v4"
-	"gorm.io/gorm"
 )
 
 type Server struct {
-	Echo   *echo.Echo
-	DB     *gorm.DB
-	Config *config.Config
+	echo *echo.Echo
 }
 
-func NewServer(
-	echo *echo.Echo,
-	db *gorm.DB,
-	config *config.Config,
-) *Server {
-	return &Server{
-		Echo:   echo,
-		DB:     db,
-		Config: config,
-	}
+func NewServer(echo *echo.Echo) *Server {
+	return &Server{echo: echo}
 }
 
 func (s *Server) Start(addr string) error {
-	if err := s.Echo.Start(":" + addr); err != nil && errors.Is(err, http.ErrServerClosed) {
+	if err := s.echo.Start(":" + addr); err != nil && errors.Is(err, http.ErrServerClosed) {
 		return fmt.Errorf("start echo: %w", err)
 	}
 
@@ -39,7 +26,7 @@ func (s *Server) Start(addr string) error {
 }
 
 func (s *Server) Shutdown(ctx context.Context) error {
-	if err := s.Echo.Shutdown(ctx); err != nil {
+	if err := s.echo.Shutdown(ctx); err != nil {
 		return fmt.Errorf("shutdown echo: %w", err)
 	}
 
